3e0bf1c0d95b0c851f474c9f20f70644c3f23c38
Mariaorostegui <mfop001new@gmail.com>
2023-07-08 20:36:23 -0500

    first part of milestone 1 testing

--- src/data.js
+++ src/data.js
@@ -23,8 +23,7 @@ function pathExists(path){
                 reject('Path does not exist', err)
             }
         });
-
-    });
+    })
 }
 
 function checkExtension(path){
@@ -42,7 +41,7 @@ function checkExtension(path){
 
 function extractLinks(path){
     return new Promise((resolve, reject) => {
-        
+
         fs.readFile(path, 'utf-8', (err, data)=>{
             if (err) {
                 console.error(err);
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,37 @@
-const mdLinks = require('../');
+const {mdLinks} = require('../src/index.js');
 
 
 describe('mdLinks', () => {
 
-  it('should...', () => {
-    console.log('FIX ME!');
+  it('should return an error when the path does not exist',()=>{
+    return mdLinks('linktes.md').catch((error)=>{
+      expect(error).toBe('Path does not exist')
+    })
   });
 
+  it('should return an error when the file is not "md"',()=>{
+    return mdLinks('linktest.txt').catch((error)=>{
+      expect(error).toEqual(error)
+    })
+  });
+
+  test('It should return the links that are in the path', () => {
+    return mdLinks('linktest.md').then(data => {
+      expect(data).toEqual([{
+        href: '(https://openai.com)',
+        text: '[OpenAI]',
+        file: '/Users/lukasarias/Documents/Laboratoria/proyecto 3/DEV009-md-links/linktest.md'
+      },
+      {
+        href: '(https://github.com)',
+        text: '[GitHub]',
+        file: '/Users/lukasarias/Documents/Laboratoria/proyecto 3/DEV009-md-links/linktest.md'
+      },
+      {
+        href: '(https://www.markdownguide.org)',
+        text: '[Markdown Guide]',
+        file: '/Users/lukasarias/Documents/Laboratoria/proyecto 3/DEV009-md-links/linktest.md'
+      }]);
+    });
+  });
 });
