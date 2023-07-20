e4a63667190fb8c821a21531439a1f72fc09a1c1
Mariaorostegui <mfop001new@gmail.com>
2023-07-20 16:03:50 -0500

    modified syntax

--- cli.js
+++ cli.js
@@ -5,27 +5,22 @@ const { statsValidate, statsLinks } = require('./data.js');
 const path = process.argv[2]
 const options = process.argv;
 
-function cli(path, options) {
-  let validate = options.includes('--validate');
-  let stats = options.includes('--stats');
+let validate = options.includes('--validate');
+let stats = options.includes('--stats');
 
-  const promise = mdLinks(path, validate).then(links => {
-    if (stats && validate) {
-      console.log(statsValidate(links));
-    } else if (stats) {
-      console.log(statsLinks(links));
-    } else if(options[3] === undefined){
-      console.log('Links found:', links);
-    }else if(validate){
-      console.log('Links found:', links)
-    }else {
-      console.log('You did not enter a valid command')
-    }
+mdLinks(path, validate).then(links => {
+  if (stats && validate) {
+    console.log(statsValidate(links));
+  } else if (stats) {
+    console.log(statsLinks(links));
+  } else if(options[3] === undefined){
+    console.log('Links found:', links);
+  }else if(validate){
+    console.log('Links found:', links)
+  }else {
+    console.log('You did not enter a valid command')
+  }
   }).catch(error => {
     console.error('Error', error);
-  });
-  return promise;
-}
-
-cli(path, options);
+});
 
